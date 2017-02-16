11e92ea3de2bafebdb399417f9b8a944e7acc61b
argus-hongjie <hongjie.zhang@rse.argus.int>
2017-02-16 12:47:25 +0100

    asfdsaf

---
+++ dep/src/main/java/dep/badClassName.java
@@ -0,0 +1,14 @@
+package dep;
+
+public class badClassName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void BadMethodName(){
+		return;
+	}
+
+}
